dd5c0ead2b9988361fa8dbfa9d65c56c31c4391c
RuslanMelnykUA <indigo211212@gmail.com>
2023-08-24 12:55:47 +0300

    fix

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,29 +1,19 @@
 import PropTypes from 'prop-types';
 import css from './Contacts.module.css';
-
-function MarkupContacts({ name, number, id, deleteContact }) {
-  return (
-    <li className={css.listItem}>
-      {name}: {number}
-      <button className={css.deleteBtn} onClick={() => deleteContact(id)}>
-        Delete
-      </button>
-    </li>
-  );
-}
+import { MarkUpContacts } from './MarkUpContacts';
 
 export const Contacts = ({ contacts, deleteContact }) => {
   return (
     <ul className={css.list}>
       {contacts.map(({ name, number, id }) => {
         return (
-          <MarkupContacts
+          <MarkUpContacts
             key={id}
             name={name}
             number={number}
             id={id}
             deleteContact={deleteContact}
-          />
+          ></MarkUpContacts>
         );
       })}
     </ul>
